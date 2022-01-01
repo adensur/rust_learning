e002c02a650664d5a83e85ac386a999913df556c
Maksim Gaiduk <adensur@gmail.com>
2022-01-01 12:12:06 +0000

    working swap function

--- swap/src/main.rs
+++ swap/src/main.rs
@@ -1,12 +1,13 @@
-fn swap(x: &mut i32, y: &mut i32) {
-    let z = x;
-    x = y;
-    y = z;
+fn swap<'a>(x: &'a mut i32, y: &'a mut i32) {
+    let z = *x;
+    *x = *y;
+    *y = z;
 }
 
 fn main() {
     let mut x = 1;
     let mut y = 2;
+    println!("x = {}, y = {}", x, y);
     swap(&mut x, &mut y);
     println!("x = {}, y = {}", x, y);
 }
