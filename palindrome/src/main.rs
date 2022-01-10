1cc0e1dd9444a1398a79da57f47e8b4053a03ac4
Maksim Gaiduk <adensur@gmail.com>
2022-01-10 15:39:48 +0000

    proper utf8 scalar values palindromes

--- palindrome/src/main.rs
+++ palindrome/src/main.rs
@@ -1,28 +1,29 @@
 use std::io;
 
-fn is_palindrome(s: &[u8]) -> bool {
-    let mut i = 0;
-    let mut j = s.len() - 1;
-    while i < j {
-        let mut ch1 = s[i];
-        let mut ch2 = s[j];
-        if (!ch1.is_ascii_alphabetic()) {
-            i += 1;
-            continue;
+fn is_palindrome(s: &str) -> bool {
+    let mut it = s.chars();
+    let mut optional1 = it.next();
+    let mut optional2 = it.next_back();
+    loop {
+        if let (Some(ch1), Some(ch2)) = (optional1, optional2) {
+            if !ch1.is_alphabetic() {
+                optional1 = it.next();
+                continue;
+            }
+            if !ch2.is_alphabetic() {
+                optional2 = it.next_back();
+                continue;
+            }
+            if ch1.to_lowercase().to_string() != ch2.to_lowercase().to_string() {
+                println!("lowercase {} != {}", ch1.to_lowercase().to_string(), ch2.to_lowercase().to_string());
+                return false;
+            }
+            optional1 = it.next();
+            optional2 = it.next_back();
+        } else {
+            return true;
         }
-        if (!ch2.is_ascii_alphabetic()) {
-            j -= 1;
-            continue;
-        }
-        ch1 = ch1.to_ascii_lowercase();
-        ch2 = ch2.to_ascii_lowercase();
-        if ch1 != ch2 {
-            return false;
-        }
-        i += 1;
-        j -= 1;
     }
-    return true;
 }
 
 #[cfg(test)]
@@ -30,23 +31,28 @@ mod tests {
     use super::*;
     #[test]
     fn test1() {
-        let mut str = String::from("asd asd");
-        assert_eq!(is_palindrome(&str.as_bytes()), false);
+        let str = String::from("asd asd");
+        assert_eq!(is_palindrome(&str), false);
     }
     #[test]
     fn test2() {
-        let mut str = String::from("asd das");
-        assert_eq!(is_palindrome(&str.as_bytes()), false);
+        let str = String::from("asd das");
+        assert_eq!(is_palindrome(&str), false);
     }
     #[test]
     fn test3() {
-        let mut str = String::from("This should be a palindrome! emOrdilapaebdlouhssiht");
-        assert_eq!(is_palindrome(&str.as_bytes()), false);
+        let str = String::from("This should be a palindrome! emOrdilapaebdlouhssiht");
+        assert_eq!(is_palindrome(&str), false);
     }
     #[test]
     fn test4() {
-        let mut str = String::from("This is not a palindrome!");
-        assert_eq!(is_palindrome(&str.as_bytes()), false);
+        let str = String::from("This is not a palindrome!");
+        assert_eq!(is_palindrome(&str), false);
+    }
+    #[test]
+    fn test5() {
+        let str = String::from("Палиндром!МОРДНИЛАП");
+        assert_eq!(is_palindrome(&str), true);
     }
 }
 
@@ -56,7 +62,7 @@ fn main() {
     io::stdin()
         .read_line(&mut s)
         .unwrap();
-    if (is_palindrome(&s.as_bytes())) {
+    if is_palindrome(&s) {
         println!("Is a palindrome!");
     } else {
         println!("Not a palindrome!");
