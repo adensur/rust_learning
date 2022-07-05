3a23e9c398e5d6444590c2ab32246d2932f7a818
adensur <adensur@gmail.com>
2022-07-05 22:40:47 +0100

    fix tests

--- shad/comm/src/main.rs
+++ shad/comm/src/main.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 use std::collections::HashSet;
-use std::io::{BufRead, BufWriter};
+use std::io::BufRead;
 
 use std::io::{self, Write};
 
@@ -17,20 +17,17 @@ fn main() {
         }
     }
     let stdout = io::stdout();
-    let handle = stdout.lock();
-    let mut buf_writer = BufWriter::new(handle);
+    let mut handle = stdout.lock();
     {
         let file = std::fs::File::open(file2).unwrap();
-        let mut reader = std::io::BufReader::new(file);
-        let mut buffer = String::new();
-        if let Ok(_) = reader.read_line(&mut buffer) {
-            //for line in reader.lines() {
-            // let line = line.unwrap();
-            if set.contains(&buffer) {
-                buf_writer.write_all(buffer.as_bytes()).unwrap();
-                set.remove(&buffer);
+        let reader = std::io::BufReader::new(file);
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if set.contains(&line) {
+                handle.write_all(line.as_bytes()).unwrap();
+                handle.write("\n".as_bytes()).unwrap();
+                set.remove(&line);
             }
-            buffer.clear();
         }
     }
 }
