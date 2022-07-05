81966ea9105c52492de3dcf82c4819b3a45990d1
adensur <adensur@gmail.com>
2022-07-05 15:50:40 +0100

    comm - compare two files line by line

---
+++ shad/comm/src/main.rs
@@ -0,0 +1,30 @@
+#![forbid(unsafe_code)]
+use std::collections::HashSet;
+use std::io::BufRead;
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let file1 = &args[1];
+    let file2 = &args[2];
+    let mut set: HashSet<String> = HashSet::new();
+    {
+        let file = std::fs::File::open(file1).unwrap();
+        let reader = std::io::BufReader::new(file);
+
+        for line in reader.lines() {
+            set.insert(line.unwrap());
+        }
+    }
+    {
+        let file = std::fs::File::open(file2).unwrap();
+        let reader = std::io::BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if set.contains(&line) {
+                println!("{}", line);
+                set.remove(&line);
+            }
+        }
+    }
+}
