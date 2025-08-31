04be2f82e6057ec2d18441685c4947bcfe9ad485
Maksim <adensur@gmail.com>
2025-08-31 12:04:01 +0100

    working binary search tree remove operation

--- 2025/2_bst/src/lib.rs
+++ 2025/2_bst/src/lib.rs
@@ -51,27 +51,117 @@ impl Bst {
     }
 
     pub fn remove(&mut self, val: i32) -> bool {
+        fn pop_min(mut node: Box<Node>) -> (i32, Option<Box<Node>>) {
+            if let Some(left) = node.left.take() {
+                let (min, sub) = pop_min(left);
+                node.left = sub;
+                return (min, Some(node));
+            }
+            return (node.value, node.right);
+        }
+
         fn remove_recursive(node: &mut Option<Box<Node>>, val: i32) -> bool {
             match node {
                 None => return false,
                 Some(n) => match val.cmp(&n.value) {
                     Ordering::Less => remove_recursive(&mut n.left, val),
                     Ordering::Greater => remove_recursive(&mut n.right, val),
-                    Ordering::Equal => {
-                        if let Some(child) = n.left.take() {
-                            *node = Some(child);
-                            true
-                        } else if let Some(child) = n.right.take() {
-                            *node = Some(child);
-                            true
-                        } else {
+                    Ordering::Equal => match (n.left.take(), n.right.take()) {
+                        (None, None) => {
                             *node = None;
                             true
                         }
-                    }
+                        (Some(child), None) => {
+                            *node = Some(child);
+                            true
+                        }
+                        (None, Some(child)) => {
+                            *node = Some(child);
+                            true
+                        }
+                        (Some(child1), Some(child2)) => {
+                            let (min, sub) = pop_min(child2);
+                            n.left = Some(child1);
+                            n.right = sub;
+                            n.value = min;
+                            true
+                        }
+                    },
                 },
             }
         }
         remove_recursive(&mut self.root, val)
+    }
+
+    pub fn to_sorted_vec(&self) -> Vec<i32> {
+        let mut results = Vec::new();
+
+        fn populate_recursive(cur: &Option<Box<Node>>, results: &mut Vec<i32>) {
+            let Some(node) = cur else {
+                return;
+            };
+            populate_recursive(&node.left, results);
+            results.push(node.value);
+            populate_recursive(&node.right, results);
+        }
+
+        populate_recursive(&self.root, &mut results);
+        results
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut tree = Bst::new();
+        assert!(!tree.contains(1));
+        tree.insert(1);
+        assert!(tree.contains(1));
+        tree.remove(1);
+        assert!(!tree.contains(1));
+    }
+
+    #[test]
+    fn test2() {
+        let mut tree = Bst::new();
+        let vec1 = tree.to_sorted_vec();
+        assert_eq!(vec1, vec![]);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(3);
+        assert_eq!(tree.to_sorted_vec(), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test3() {
+        let mut tree = Bst::new();
+        for i in 0..=10 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.to_sorted_vec(), vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        tree.remove(5);
+        assert_eq!(tree.to_sorted_vec(), vec![0, 1, 2, 3, 4, 6, 7, 8, 9, 10]);
+        tree.remove(3);
+        assert_eq!(tree.to_sorted_vec(), vec![0, 1, 2, 4, 6, 7, 8, 9, 10]);
+        tree.remove(2);
+        assert_eq!(tree.to_sorted_vec(), vec![0, 1, 4, 6, 7, 8, 9, 10]);
+        tree.remove(9);
+        assert_eq!(tree.to_sorted_vec(), vec![0, 1, 4, 6, 7, 8, 10]);
+    }
+
+    #[test]
+    fn test4() {
+        let mut tree = Bst::new();
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(6);
+        tree.insert(8);
+        assert_eq!(tree.to_sorted_vec(), vec![2, 5, 6, 7, 8]);
+        tree.remove(5);
+        assert_eq!(tree.to_sorted_vec(), vec![2, 6, 7, 8]);
     }
 }
